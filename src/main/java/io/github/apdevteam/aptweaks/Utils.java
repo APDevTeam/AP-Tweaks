69005f4f8b82a849d0d3e49d084edaf22d82088b
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-07-03 11:33:13 -0500

    Unbreaking works

---
+++ src/main/java/io/github/apdevteam/aptweaks/Utils.java
@@ -0,0 +1,62 @@
+package io.github.apdevteam.aptweaks;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Utils {
+    public static boolean hasEnchantment(@Nullable ItemStack item, @NotNull Enchantment enchantment) {
+        if (item == null || item.getItemMeta() == null)
+            return false;
+        if (item.containsEnchantment(enchantment))
+            return true; // Simple enchantment
+
+        // Enchanted books have a special type of metadata for their enchantments
+        ItemMeta meta = item.getItemMeta();
+        if (!(meta instanceof EnchantmentStorageMeta))
+            return false;
+
+        EnchantmentStorageMeta enchantmentStorageMeta = (EnchantmentStorageMeta) meta;
+        return enchantmentStorageMeta.hasStoredEnchant(enchantment);
+    }
+
+    public static int enchantmentLevel(@Nullable ItemStack item, @NotNull Enchantment enchantment) {
+        if (item == null || item.getItemMeta() == null)
+            return 0;
+        if (item.containsEnchantment(enchantment))
+            return item.getEnchantmentLevel(enchantment); // Simple enchantment
+
+        // Enchanted books have a special type of metadata for their enchantments
+        ItemMeta meta = item.getItemMeta();
+        if (!(meta instanceof EnchantmentStorageMeta))
+            return 0;
+
+        EnchantmentStorageMeta enchantmentStorageMeta = (EnchantmentStorageMeta) meta;
+        return enchantmentStorageMeta.getStoredEnchantLevel(enchantment);
+    }
+
+    public static void setEnchantment(@Nullable ItemStack item, @NotNull Enchantment enchantment, int level) {
+        if (item == null || item.getItemMeta() == null)
+            return;
+        if (item.containsEnchantment(enchantment)) {
+            // Simple enchantment
+            item.removeEnchantment(enchantment);
+            item.addUnsafeEnchantment(enchantment, level);
+            return;
+        }
+
+        // Enchanted books have a special type of metadata for their enchantments
+        ItemMeta meta = item.getItemMeta();
+        if (!(meta instanceof EnchantmentStorageMeta))
+            return;
+
+        EnchantmentStorageMeta enchantmentStorageMeta = (EnchantmentStorageMeta) meta;
+        if (enchantmentStorageMeta.hasStoredEnchant(enchantment))
+            enchantmentStorageMeta.removeStoredEnchant(enchantment);
+        enchantmentStorageMeta.addStoredEnchant(enchantment, level, true);
+        item.setItemMeta(enchantmentStorageMeta);
+    }
+}
--- src/main/java/io/github/apdevteam/aptweaks/tweaks/UnbreakingV.java
+++ src/main/java/io/github/apdevteam/aptweaks/tweaks/UnbreakingV.java
@@ -1,5 +1,6 @@
 package io.github.apdevteam.aptweaks.tweaks;
 
+import io.github.apdevteam.aptweaks.Utils;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,26 +9,29 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class UnbreakingV implements Listener {
-    public static int MAX_LEVEL = 3;
+    public static int MAX_VANILLA_LEVEL = 3;
 
     @EventHandler
     public void onAnvilEvent(@NotNull PrepareAnvilEvent event) {
+        ItemStack result = event.getResult();
+        if (result == null || result.getItemMeta() == null || !Utils.hasEnchantment(result, Enchantment.DURABILITY))
+            return;
+        if (Utils.enchantmentLevel(result, Enchantment.DURABILITY) != MAX_VANILLA_LEVEL)
+            return; // We only need to correct when the result is the max vanilla level
+
         int max = -1;
         for (ItemStack item : event.getInventory().getContents()) {
-            if(item.getEnchantmentLevel(Enchantment.DURABILITY) > MAX_LEVEL) {
-                max = item.getEnchantmentLevel(Enchantment.DURABILITY);
-                break;
-            }
+            if (item == null || item.getItemMeta() == null || !Utils.hasEnchantment(item, Enchantment.DURABILITY))
+                continue;
+            int unbreakingLevel = Utils.enchantmentLevel(item, Enchantment.DURABILITY);
+            if (unbreakingLevel > max)
+                max = unbreakingLevel;
         }
-        if (max != -1)
-            return;
+        if (max == -1 || max <= MAX_VANILLA_LEVEL)
+            return; // We were unable to detect a source item with unbreaking or only detected vanilla levels
 
-        ItemStack result = event.getResult();
-        if (result.getEnchantmentLevel(Enchantment.DURABILITY) == max)
-            return; // already correct
-
-        // set max level
-        result.addUnsafeEnchantment(Enchantment.DURABILITY, max);
+        // Set result level
+        Utils.setEnchantment(result, Enchantment.DURABILITY, max);
         event.setResult(result);
     }
 }
