69e4c57afa9497b180e8551cf335d1ed722d2dc0
TylerS1066 <TylerS1066@users.noreply.github.com>
2026-06-26 17:50:23 -0500

    Update Maven repository URL in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/apdevteam/movecraft-coreprotect")
+            url = uri("https://maven.pkg.github.com/apdevteam/ap-tweaks")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
