bec65817e80a0cb686a98721370bf21933710998
TylerS1066 <TylerS1066@users.noreply.github.com>
2026-06-26 17:42:42 -0500

    Gradle, ci, & more (#2)
    * Switch to gradle, add CI and dependabot
    
    * Pull version from git
    
    * Update to 1.20.6
    
    * Fix gradlew

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.github.apdevteam.github-packages") version "1.2.2"
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven { githubPackage("apdevteam/movecraft")(this) }
+}
+
+dependencies {
+    api("org.jetbrains:annotations-java5:24.1.0")
+    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("net.countercraft:movecraft:+")
+}
+
+group = "io.github.apdevteam"
+version = System.getenv("RELEASE_VERSION")?.takeIf { it.isNotBlank() }
+    ?: runCatching {
+        val sha = ProcessBuilder("git", "rev-parse", "--short", "HEAD")
+            .start().inputStream.bufferedReader().readLine() ?: "unknown"
+        val tag = ProcessBuilder("git", "describe", "--tags", "--abbrev=0")
+            .start().inputStream.bufferedReader().readLine() ?: "untagged"
+        val dirty = ProcessBuilder("git", "status", "--porcelain")
+            .start().inputStream.bufferedReader().readLine() != null
+        if (dirty) "$tag+$sha-dirty" else "$tag+$sha"
+    }.getOrElse { "unknown" }
+description = "AP-Tweaks"
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+tasks.jar {
+    archiveBaseName.set("AP-Tweaks")
+    archiveClassifier.set("")
+    archiveVersion.set("")
+}
+
+tasks.processResources {
+    from(rootProject.file("LICENSE.md"))
+    filesMatching("*.yml") {
+        expand(mapOf("projectVersion" to project.version))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "io.github.apdevteam"
+            artifactId = "AP-Tweaks"
+            version = "${project.version}"
+
+            artifact(tasks.jar)
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/apdevteam/movecraft-coreprotect")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
--- src/main/java/io/github/apdevteam/aptweaks/tweaks/InfinityMending.java
+++ src/main/java/io/github/apdevteam/aptweaks/tweaks/InfinityMending.java
@@ -2,10 +2,9 @@ package io.github.apdevteam.aptweaks.tweaks;
 
 import io.github.apdevteam.aptweaks.Utils;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
@@ -15,44 +14,49 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class InfinityMending implements Listener {
+    private final Logger logger;
+
+    public InfinityMending(Logger logger) {
+        this.logger = logger;
+    }
+
     @EventHandler
     public void onAnvilEvent(@NotNull PrepareAnvilEvent event) {
         ItemStack result = event.getResult();
         if (result != null && result.getType() != Material.AIR) {
-            Bukkit.getLogger().info("Result: " + result.getType());
+            // logger.info("Result: " + result.getType());
             return;
         }
 
         ItemStack[] contents = event.getInventory().getContents();
         if (contents.length != 2) {
-            Bukkit.getLogger().info("Contents: " + contents.length);
+            // logger.info("Contents: " + contents.length);
             return; // Only allow two items in the anvil
         }
 
         ItemStack one = contents[0];
         ItemStack two = contents[1];
         if (one == null || two == null) {
-            Bukkit.getLogger().info("One: " + one + " Two: " + two);
+            // logger.info("One: " + one + " Two: " + two);
             return; // Both items must be present
         }
-        if (!Utils.hasEnchantment(one, Enchantment.ARROW_INFINITE)
-                && !Utils.hasEnchantment(two, Enchantment.ARROW_INFINITE)) {
-            Bukkit.getLogger().info("One: " + Utils.hasEnchantment(one, Enchantment.ARROW_INFINITE) + " Two: " + Utils.hasEnchantment(two, Enchantment.ARROW_INFINITE));
+        if (!Utils.hasEnchantment(one, Enchantment.INFINITY)
+                && !Utils.hasEnchantment(two, Enchantment.INFINITY)) {
+            // logger.info("One: " + Utils.hasEnchantment(one, Enchantment.INFINITY) + " Two: " + Utils.hasEnchantment(two, Enchantment.INFINITY));
             return; // Require one of the items to have the infinity enchantment
         }
         if (!Utils.hasEnchantment(one, Enchantment.MENDING)
                 && !Utils.hasEnchantment(two, Enchantment.MENDING)) {
-            Bukkit.getLogger().info("One: " + Utils.hasEnchantment(one, Enchantment.MENDING) + " Two: " + Utils.hasEnchantment(two, Enchantment.MENDING));
+            // logger.info("One: " + Utils.hasEnchantment(one, Enchantment.MENDING) + " Two: " + Utils.hasEnchantment(two, Enchantment.MENDING));
             return; // Require one of the items to have the mending enchantment
         }
         if (one.getType() != Material.ENCHANTED_BOOK && one.getType() != Material.BOW) {
-            Bukkit.getLogger().info("One: " + one.getType());
+            // logger.info("One: " + one.getType());
             return; // Only allow enchanted books or bows in the first slot of the anvil
         }
         if (two.getType() != Material.ENCHANTED_BOOK) {
-            Bukkit.getLogger().info("Two: " + two.getType());
+            // logger.info("Two: " + two.getType());
             return; // Only allow enchanted books in the second slot of the anvil
-            // TODO: Expand this to be more flexible
         }
 
         Map<Enchantment, Integer> resultEnchants = Utils.combine(Utils.getEnchantments(one), Utils.getEnchantments(two));        
@@ -62,8 +66,8 @@ public class InfinityMending implements Listener {
         }
         event.setResult(result);
         event.getInventory().setRepairCost(1);
-        Bukkit.getLogger().info(
-            "Result: " + event.getResult().getType()
+        logger.info(
+            "Infinity Mending Result: " + event.getResult().getType()
             + " Repair Cost: " + event.getInventory().getRepairCost()
             + " Enchantments: " + Utils.getEnchantments(event.getResult()).size()
         );
